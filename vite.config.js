e95bdd0515ff94b8d1c571c08c0292d173d3f501
wkl <2636733703@qq.com>
2023-06-12 21:20:57 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/vue3/",
+  base:"/vues3/",
   build:{
     outDir:"docs"
   },
